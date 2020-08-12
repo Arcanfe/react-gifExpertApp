40b152d1c83fcc923b5c3ce5f661a0843c2c7318
Andres Ariza <afelipe2599@gmail.com>
2020-08-12 15:55:42 -0500

    docs agregado en vez de build

---
+++ docs/precache-manifest.b01cdefa426a3168c043f0a2205d2473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ec723e8a5bd58e59159f2dcc9961ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "225aa3583d574ad16d90",
+    "url": "/static/css/main.f4469d01.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225aa3583d574ad16d90",
+    "url": "/static/js/main.f2d44cc0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
